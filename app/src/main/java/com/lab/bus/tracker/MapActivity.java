065028cc1242092b0c6838dbb4706e8ea4eef1d0
deep-154 <deepankar11093@gmail.com>
2016-04-01 09:52:50 +0530

    client shift to asyncTask

--- app/src/main/java/com/lab/bus/tracker/MapActivity.java
+++ app/src/main/java/com/lab/bus/tracker/MapActivity.java
@@ -1,5 +1,6 @@
 package com.lab.bus.tracker;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,26 +11,36 @@ import android.util.Log;
  */
 public class MapActivity extends AppCompatActivity implements SocketClient.OnMessageReceiveListener{
 
-    Thread clientThread;
-
+    SocketClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        final SocketClient client = new SocketClient(this);
+        client = new SocketClient(this);
 
-        clientThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                client.run();
-            }
-        });
-        clientThread.run();
+        new TcpTask().execute();
     }
 
     @Override
     public void messageReceived(String message) {
-        Log.e("RESPONSE FROM SERVER", "S: Received Message: " + message);
+        Log.e("RESPONSE FROM SERVER", "" + message);
+    }
+
+    private class TcpTask extends AsyncTask<Void, Void, Void> {
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            client.run();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+        }
+
     }
 }
--- app/src/main/java/com/lab/bus/tracker/SocketClient.java
+++ app/src/main/java/com/lab/bus/tracker/SocketClient.java
@@ -17,14 +17,11 @@ import java.net.Socket;
 
 public class SocketClient {
 
-    private String serverMessage;
-    public static final String SERVERIP = "Your IP Address";
-    public static final int SERVERPORT = 4444;
+    public static final String SERVERIP = "172.25.36.48";
+    public static final int SERVERPORT = 12345;
     private OnMessageReceiveListener mMessageListener = null;
     private boolean mRun = false;
-
     private PrintWriter out;
-    private BufferedReader in;
 
 
     public SocketClient(OnMessageReceiveListener listener) {
@@ -49,35 +46,29 @@ public class SocketClient {
     public void run() {
 
         mRun = true;
+        String serverMessage;
 
         try {
-            //here you must put your computer's IP address.
+
             InetAddress serverAddr = InetAddress.getByName(SERVERIP);
-
-            Log.e("TCP Client", "C: Connecting...");
-
-            //create a socket to make the connection with the server
             Socket socket = new Socket(serverAddr, SERVERPORT);
 
             try {
-
                 //send the message to the server
                 out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
                 Log.e("TCP Client", "C: Done.");
 
                 //receive the message which the server sends back
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
                 //in this while the client listens for the messages sent by the server
                 while (mRun) {
-                    serverMessage = in.readLine();
+                   serverMessage = in.readLine();
 
                     if (serverMessage != null && mMessageListener != null) {
                         mMessageListener.messageReceived(serverMessage);
                     }
-                    serverMessage = null;
-
                 }
 
             } catch (Exception e) {
@@ -85,8 +76,6 @@ public class SocketClient {
                 Log.e("TCP", "S: Error", e);
 
             } finally {
-                //the socket must be closed. It is not possible to reconnect to this socket
-                // after it is closed, which means a new socket instance has to be created.
                 socket.close();
             }
 
